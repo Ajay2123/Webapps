731bf4ce5790750b6fb793fac88cdd32e7166ff3
ajay kumar Rajamundry <ajayraj2187@gmail.com>
2019-02-01 09:55:05 +0100

    not able to console font repository

--- src/index.js
+++ src/index.js
@@ -3,23 +3,31 @@
 
    var
       router = require('router'),
-      appData = require('appData');
-
-
-   router.get('/', function (req, res) {
-      const UNIQUE_ID = new Date().getTime();
-
-      var startNumber = appData.get('startNumber'),
+      ResourceLocatorUtil = require('ResourceLocatorUtil'),
+      appData = require('appData'),
+      fontRepository = ResourceLocatorUtil.getFontRepository();
+      
+      router.get('/', function (req, res) {
+         const UNIQUE_ID = new Date().getTime();
+         
+         
+         var startNumber = appData.get('startNumber'),
          endNumber = appData.get('endNumber'),
          duration = appData.get('duration'),
          css = appData.get('css');
+         font= fontRepository.getSelectorName();
+         
+
+         
 
       res.render('/', {
          UNIQUE_ID: UNIQUE_ID,
-         startNumber:startNumber,
+         startNumber: startNumber,
          endNumber: endNumber,
          duration: duration,
-         css: css
+         css: css,
+         font:font,
+         
 
       });
    });
--- src/main.js
+++ src/main.js
@@ -1,26 +1,32 @@
-define(function(require) {
+define(function (require) {
    'use strict';
 
    var
-      _          = require('underscore'),
-      Component  = require('Component'),
-      template   = require('/template/main');
+      _ = require('underscore'),
+      Component = require('Component'),
+      template = require('/template/main');
 
-      return Component.extend({
-         template: template,
-         
-         filterState: function(state) {
-       
-            return _.extend({}, {
-
-               UNIQUE_ID: state.UNIQUE_ID,
-               startNumber:state.startNumber,
-               endNumber: state.endNumber,
-               duration: state.duration,
-               css: state.css
-      
-            });
+   return Component.extend({
+      onRendered: function () {
+/* 
+         for (i = 0; i < fontSize; i++) {
+            console.log('fontList[', i, ']=', fontList[i]);
          }
-      });
+ */
+      },
+      template: template,
+
+      filterState: function (state) {
+
+         return _.extend({}, {
+
+            UNIQUE_ID: state.UNIQUE_ID,
+            startNumber: state.startNumber,
+            endNumber: state.endNumber,
+            duration: state.duration,
+            css: state.css,
+            font: state.font,
+         });
+      }
    });
-   
+});
